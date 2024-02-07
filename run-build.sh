e615e81a114643ef41e7d550e3c316cbdc800651
bessavagner <bessavagner@gmail.com>
2024-02-07 16:25:50 -0300

    fix: add tailwindcss plugins isntallation to build

--- run-build.sh
+++ run-build.sh
@@ -4,5 +4,8 @@
 python build.py
 # Install tailwindcss
 npm install -D tailwindcss
+npm install -D @tailwindcss/typography
+npm install -D @tailwindcss/typography
+npm i -D daisyui@latest
 # Start the Tailwind CLI build process
 npx tailwindcss -i ./static/css/input.css -o ./static/css/styles.css
