861e9c5d8f112177db8d9ae424356e90119b4522
bessavagner <bessavagner@gmail.com>
2024-02-07 19:02:46 -0300

    fix: add enviroment variables

--- run-build.sh
+++ run-build.sh
@@ -2,11 +2,15 @@
 
 # Process Markdown files and parse to json
 pip install -r requirements.txt
-python build.py
+
 # Install tailwindcss
 npm install tailwindcss
 npm install @tailwindcss/typography
 npm install @tailwindcss/typography
 npm install daisyui@latest
+
+# build files to serve
+python build.py
+
 # Start the Tailwind CLI build process
 npx tailwindcss -i ./static/css/input.css -o ./static/css/styles.css
--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import uvicorn
 
-# load_dotenv()  # Loads variables from .env file
+load_dotenv()  # Loads variables from .env file
 
-APP_NAME = os.getenv("APP_NAME", default="app")
-APP_DIR = os.getenv("APP_DIR", default=".")
-APP_PORT = int(os.getenv("APP_PORT", default="5000"))
-ALLOWED_HOSTS = os.getenv(
+APP_NAME = os.environ.get("APP_NAME", default="app")
+APP_DIR = os.environ.get("APP_DIR", default=".")
+APP_PORT = int(os.environ.get("APP_PORT", default="5000"))
+ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS", default="localhost,0.0.0.0"
 ).split(',')
 HOST = ALLOWED_HOSTS[1]
+DEBUG = bool(os.environ.get("DEBUG", default=True))
 
 if __name__ == '__main__':
     uvicorn.run(
@@ -18,7 +19,7 @@ if __name__ == '__main__':
         port=APP_PORT,
         app_dir=APP_DIR,
         host=HOST,
-        reload=True,
+        reload=DEBUG,
         reload_includes=[
             "templates/*.html",
             "templates/**/*.html",
