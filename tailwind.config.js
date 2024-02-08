297c221210507a876712da76d0ec763010423889
bessavagner <bessavagner@gmail.com>
2024-02-08 08:18:13 -0300

    fix: set custom shadow-3xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./static/**/*.{html,.js,css}", "./templates/**/*.{html,.js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 10px 15px 10px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   daisyui: {
     themes: ["light", "dark", "cupcake"],
